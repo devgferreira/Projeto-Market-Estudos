c87cb9d645e4249a8978cada6a1da45b0683d9d3
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-08-01 19:21:27 -0300

    Feat: Implementando o get e set no Employee

--- src/Employee.java
+++ src/Employee.java
@@ -1,8 +1,28 @@
 public class Employee {
-    String Name;
-    int Age;
-    Section Section;
+    private String Name;
+    private int Age;
+    private Section Section;
 
-    
+    public Employee(String name, int age, Section section) {
+        Name = name;
+        Age = age;
+        Section = section;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
 
 }
